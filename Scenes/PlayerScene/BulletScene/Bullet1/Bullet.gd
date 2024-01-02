e5ea5bad08f0239c77c1fe98d2a859eba6802d85
Holocron <108415700+Ho1ocron@users.noreply.github.com>
2024-01-02 20:35:06 +0500

    Add files via upload

---
+++ Scenes/PlayerScene/BulletScene/Bullet1/Bullet.gd
@@ -0,0 +1,26 @@
+extends Area2D
+
+@export var speed = 1500
+
+var screen_size
+var damage = 1
+
+
+@onready var angle_in_radians = get_angle_to(get_global_mouse_position())
+
+func _ready():
+	screen_size = get_viewport_rect().size
+
+
+func quit():
+	print("I quit")
+	queue_free()
+	
+
+func _process(delta):
+	$Sprite2D.rotate(1)
+	position += Vector2(1, 0).rotated(angle_in_radians) * speed * delta
+
+
+func _on_on_screen_screen_exited():
+	queue_free()
