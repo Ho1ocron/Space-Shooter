6fa0fb6da8df0183d5cc41adffb390eed29b8a59
Ho1ocron <cmrfreeze@gmail.com>
2024-02-18 18:24:08 +0500

    added a bit more features

--- scripts/test_player/test_player.gd
+++ scripts/test_player/test_player.gd
@@ -10,6 +10,9 @@ func _ready():
 
 
 func moving(delta):
+	if not Input.is_action_pressed("StopRotation"):
+		rotation = lerp_angle(rotation, (get_viewport().get_mouse_position() - global_position).angle()+PI/2, $ship.rotation_speed) #: Rotating player if button Alt isn't being pressed
+		 
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	input_vector.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
