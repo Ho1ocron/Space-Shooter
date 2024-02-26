937f85329117d4e40d437a71f495687fe33bd13f
Ho1ocron <cmrfreeze@gmail.com>
2024-02-26 21:51:43 +0500

    tet_8

--- scripts/ship.cpp
+++ scripts/ship.cpp
@@ -21,14 +21,14 @@ Ship::~Ship() {
 
 void Ship::_process(double delta) {
     time_passed += delta;
+    if (!is_inside_tree()) {
+        // Ship node is not in the scene
+        return;
+    }
 
     Input* input = Input::get_singleton();
 
-    if (input->is_key_pressed(KEY_S)) {
-        set_position(Vector2(10.0 + (10.0 * sin(time_passed * 2.0)), 10.0 + (10.0 * cos(time_passed * 2.0))));
-    }
+    if (input->is_key_pressed(KEY_A)) { set_position(Vector2(10.0 + (10.0 * sin(time_passed * 2.0)), 10.0 + (10.0 * cos(time_passed * 2.0)))); }
 
-    if (input->is_key_pressed(KEY_D)) {
-        set_position(Vector2(576.0, 324.0));
-    }
+    if (input->is_key_pressed(KEY_S)) { set_position(Vector2(576.0, 324.0)); }
 }
