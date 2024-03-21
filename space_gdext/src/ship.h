baacb9adfd4bcd31d898c811bd5848eeab151845
litarin1 <butyrinalek@yandex.ru>
2024-03-21 14:00:54 +0700

    добавил форматирование

--- space_gdext/src/ship.h
+++ space_gdext/src/ship.h
@@ -2,35 +2,35 @@
 #define SHIP_CLASS_H
 
 #include <godot_cpp/classes/character_body2d.hpp>
-#include "health_component.h"
 #include <memory>
+
+#include "health_component.h"
 
 using namespace godot;
 
-struct Specifications{
+struct Specifications {
     unsigned int speed;
     unsigned int rotation_speed;
 
-    Specifications(unsigned int speed, unsigned int rotation_speed) : speed(speed), rotation_speed(rotation_speed){}
+    Specifications(unsigned int speed, unsigned int rotation_speed) : speed(speed), rotation_speed(rotation_speed) {}
 };
 
-class ShipBase : public CharacterBody2D{
+class ShipBase : public CharacterBody2D {
     GDCLASS(ShipBase, CharacterBody2D)
 
-    public:
-        Ref<HealthComponent> health;
-        // Specifications specifications;
+public:
+    Ref<HealthComponent> health;
+    // Specifications specifications;
 
-        Ref<HealthComponent> get_health_component();
-        void set_health_component(const godot::Ref<HealthComponent>&);
+    Ref<HealthComponent> get_health_component();
+    void set_health_component(const godot::Ref<HealthComponent>&);
 
-        // ShipBase(std::shared_ptr<HealthComponent> health);
-        ShipBase();
-        ~ShipBase();
-    
-    protected:
-        static void _bind_methods();
+    // ShipBase(std::shared_ptr<HealthComponent> health);
+    ShipBase();
+    ~ShipBase();
+
+protected:
+    static void _bind_methods();
 };
 
-
-#endif // SHIP_CLASS_H
+#endif  // SHIP_CLASS_H
